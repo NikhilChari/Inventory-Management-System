91dd7a71c955abd573237ad68dcec219ad1946b2
Hruday <mscids2024.hruday@unigoa.ac.in>
2025-09-10 18:43:32 +0530

    Setup basic structure and constants

---
+++ script.js
@@ -0,0 +1,4 @@
+const apiUrl = 'http://localhost:5500/api/items';
+const tableBody = document.querySelector('#inventoryTable tbody');
+const addForm = document.getElementById('addForm');
+const messageDiv = document.getElementById('message');
